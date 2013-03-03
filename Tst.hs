b104a48388fecf34ce82f3023c8b1bda0fbf7299
Yi Dai <plmday@gmail.com>
2013-03-04 00:54:15 +0100

    Refactored a bit

--- Tst.hs
+++ Tst.hs
@@ -2,23 +2,23 @@ import Prelude hiding (all)
 import UTT
 
 
-tm1  = als [("x", typ 0)] (var "x")
-tps1 = [ als [("x", typ 0)] (typ 0)
+tm1  = all ("x", typ 0) (var "x")
+tps1 = [ all ("x", typ 0) (typ 0)
        , typ 0 ]
 
 tm2  = als [("x", typ 0), ("y", typ 0)] (var "y")
 tps2 = [ als [("x", typ 0), ("y", typ 0)] (typ 0)
-       , als [("x", typ 0)] (typ 0)
+       , all ("x", typ 0) (typ 0)
        , typ 0 ]
 
-tm3  = als [ ("x1", typ 0), ("x2", als [("y", nat)] (typ 0))
-           , ("x3", nat), ("x4", aps (var "x2") [var "x3"]) ]
-           (aps (var "x2") [(var "x3")])
-tps3 = [ als [ ("x1", typ 0), ("x2", als [("y", nat)] (typ 0))
-             , ("x3", nat), ("x4", aps (var "x2") [var "x3"]) ]
+tm3  = als [ ("x1", typ 0), ("x2", all ("y", nat) (typ 0))
+           , ("x3", nat), ("x4", app (var "x2") (var "x3")) ]
+           (app (var "x2") (var "x3"))
+tps3 = [ als [ ("x1", typ 0), ("x2", all ("y", nat) (typ 0))
+             , ("x3", nat), ("x4", app (var "x2") (var "x3")) ]
              (typ 0)
-       , als [("x1", typ 0), ("x2", als [("y", nat)] (typ 0)), ("x3", nat)] (typ 0)
-       , als [("x1", typ 0), ("x2", als [("y", nat)] (typ 0))] (typ 0)
-       , als [("x1", typ 0)] (typ 0)
+       , als [("x1", typ 0), ("x2", all ("y", nat) (typ 0)), ("x3", nat)] (typ 0)
+       , als [("x1", typ 0), ("x2", all ("y", nat) (typ 0))] (typ 0)
+       , all ("x1", typ 0) (typ 0)
        , typ 0 ]
 
